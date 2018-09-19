ff8cd2ac16674d6b2abb19656f5e39b069fb8d07
rauldoe <khoado@csu.fullerton.edu>
2018-09-19 09:35:29 -0700

    cleanup

--- proj1.py
+++ proj1.py
@@ -5,23 +5,13 @@ from flask import Flask
 from flask import g
 from flask import request
 from flask import jsonify
-from flask import make_response
 from forum import forum
 from forumList import forumList
 
-Ok = 200
-Created = 201
-
-GET = "GET"
-POST = "POST"
-
-forumsUrl = "/forums"
-forumsFromIdUrl = forumsUrl + "/<int:id>"
-
 app = Flask(__name__)
 
-@app.route(forumsUrl, methods=[GET])
-def forumsGet():
+@app.route("/forums")
+def forums():
 
     list = forumList()
 
@@ -29,19 +19,9 @@ def forumsGet():
     list.appendItem(2, "name2", "creator2")
     list.appendItem(3, "name3", "creator3")
 
-    return make_response(list.serialize(), Ok)
+    return list.serialize()
 
-@app.route(forumsUrl, methods=[POST])
+@app.route("/forums", methods=["POST"])
 def forumsPost():
-    return make_response(forum.deserialize(request.json).serializeJson(), Created)
-
-@app.route(forumsFromIdUrl, methods=[GET])
-def forumsFromIdGet(id):
-
-    list = forumList()
-
-    list.appendItem(1, "name1", "creator1")
-    list.appendItem(2, "name2", "creator2")
-    list.appendItem(3, "name3", "creator3")
-
-    return make_response(list.find(1).serialize(), Ok)
+    #return forum(0, "test", "").serialize(), 201
+    return jsonify(forum.deserialize(request.json).serialize()), 201
