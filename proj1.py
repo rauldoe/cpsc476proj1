50582b93fe67b6c97412e8aa1ff144ba2bf1077b
rauldoe <khoado@csu.fullerton.edu>
2018-09-19 06:35:44 -0700

    update of serialize code

--- proj1.py
+++ proj1.py
@@ -5,13 +5,23 @@ from flask import Flask
 from flask import g
 from flask import request
 from flask import jsonify
+from flask import make_response
 from forum import forum
 from forumList import forumList
 
+Ok = 200
+Created = 201
+
+GET = "GET"
+POST = "POST"
+
+forumsUrl = "/forums"
+forumsFromIdUrl = forumsUrl + "/<int:id>"
+
 app = Flask(__name__)
 
-@app.route("/forums")
-def forums():
+@app.route(forumsUrl, methods=[GET])
+def forumsGet():
 
     list = forumList()
 
@@ -19,9 +29,19 @@ def forums():
     list.appendItem(2, "name2", "creator2")
     list.appendItem(3, "name3", "creator3")
 
-    return list.serialize()
+    return make_response(list.serialize(), Ok)
 
-@app.route("/forums", methods=["POST"])
+@app.route(forumsUrl, methods=[POST])
 def forumsPost():
-    #return forum(0, "test", "").serialize(), 201
-    return jsonify(forum.deserialize(request.json).serialize()), 201
+    return make_response(forum.deserialize(request.json).serializeJson(), Created)
+
+@app.route(forumsFromIdUrl, methods=[GET])
+def forumsFromIdGet(id):
+
+    list = forumList()
+
+    list.appendItem(1, "name1", "creator1")
+    list.appendItem(2, "name2", "creator2")
+    list.appendItem(3, "name3", "creator3")
+
+    return make_response(list.find(1).serialize(), Ok)
