f495e15be78546964e432ee89816737abe649035
rauldoe <khoado@csu.fullerton.edu>
2018-09-20 06:45:22 -0700

    new objects

---
+++ threadConversation.py
@@ -0,0 +1,36 @@
+import datetime
+from flask import jsonify
+
+class threadConversation:
+    id = 0
+    forum_id = 0
+    title = ""
+    text1 = ""
+    author = ""
+    timestamp1 = datetime.datetime.now()
+
+    def __init__(self, id, forum_id, title, text1, author, timestamp1):
+        self.id = id
+        self.forum_id = forum_id
+        self.title = title
+        self.text1 = text1
+        self.author = author
+        self.timestamp1 = timestamp1
+
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'forum_id': self.forum_id,
+            'title': self.title,
+            'text1': self.text1,
+            'author': self.author,
+            'timestamp1': self.timestamp1
+        }
+    
+    def serializeJson(self):
+        return jsonify(self.serialize())
+
+    @staticmethod
+    def deserialize(jsonObj):
+        return threadConversation(0, jsonObj["forum_id"], jsonObj["title"],jsonObj["text1"],jsonObj["author"],jsonObj["timestamp1"])
+
