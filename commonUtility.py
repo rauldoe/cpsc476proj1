6bfe0c684633eaaf3083c760c8e99562a94b32c7
rauldoe <khoado@csu.fullerton.edu>
2018-09-20 20:04:18 -0700

    exist check

--- commonUtility.py
+++ commonUtility.py
@@ -1,4 +1,8 @@
 
+from flask import abort
+
+from db import db
+
 class commonUtility:
 
     @staticmethod
@@ -7,3 +11,31 @@ class commonUtility:
             return dict[key]
         else:
             return None
+
+    @staticmethod
+    def ifExistDoError(dbPath, query, errorStatus):
+        return commonUtility.checkIfExist(dbPath, query, errorStatus, 0)
+
+    @staticmethod
+    def ifNotExistDoError(dbPath, query, errorStatus):
+        return commonUtility.checkIfExist(dbPath, query, errorStatus, 1)
+
+    @staticmethod
+    def checkIfExist(dbPath, query, errorStatus, statusWhenExist):
+
+        isPassed = True
+
+        conn = db.initDb(dbPath)
+        doesExist = db.executeIfExist(conn, query)
+        db.closeDb(conn)
+
+        if statusWhenExist == 0:
+            if doesExist:
+                isPassed = False
+                abort(errorStatus)
+        elif statusWhenExist == 1:
+            if not doesExist:
+                isPassed = False
+                abort(errorStatus)
+        
+        return isPassed
