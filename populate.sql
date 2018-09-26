8793a963751de9e77189c0f43665a312e592c24d
yashdeepk <yashdeepk@csu.fullerton.edu>
2018-09-25 20:37:16 -0700

    db changes

--- populate.sql
+++ populate.sql
@@ -1,17 +1,21 @@
 
-INSERT INTO users(username, password) VALUES ('pd', 'pd1');
-INSERT INTO users(username, password) VALUES ('ed', 'ed1');
+INSERT INTO users(username, password) VALUES ('yashdeep', 'password123');
+INSERT INTO users(username, password) VALUES ('tester', 'tester123');
+INSERT INTO users(username, password) VALUES ('test', 'test');
+INSERT INTO users(username, password) VALUES ('david', 'abc');
 
 
-INSERT INTO forums(name, creator) VALUES ('first forum', 'cd343');
-INSERT INTO forums(name, creator) VALUES ('second forum', 'md2323');
+INSERT INTO forums(name, creator) VALUES ('sample forum', 'yashdeep');
+INSERT INTO forums(name, creator) VALUES ('lets try', 'tester');
+INSERT INTO forums(name, creator) VALUES ('what we need to do', 'david');
 
-INSERT INTO threadConversations(forum_id, title, text, author, timestamp) VALUES (1, 'first thread - first forum', 'hey this is great', 'paul', date('now'));
-INSERT INTO threadConversations(forum_id, title, text, author, timestamp) VALUES (1, 'second thread - first forum', 'hey this is greatsdddddd', 'paul schloe', date('now'));
-INSERT INTO threadConversations(forum_id, title, text, author, timestamp) VALUES (2, 'first thread - second forum', 'hey this is not bad', 'steve', date('now'));
-INSERT INTO threadConversations(forum_id, title, text, author, timestamp) VALUES (2, 'second thread - second forum', 'hey this is not basaddddd', 'steveiebe', date('now'));
+INSERT INTO threads(forum_id, title, text, author, timestamp) VALUES (1, 'this is first try', 'how you do that', 'tester', date('now'));
+INSERT INTO threads(forum_id, title, text, author, timestamp) VALUES (1, 'this is second try', 'how backend works', 'david', date('now'));
+INSERT INTO threads(forum_id, title, text, author, timestamp) VALUES (2, 'try once more lets see', 'how to do software testing', 'test', date('now'));
+INSERT INTO threads(forum_id, title, text, author, timestamp) VALUES (2, 'again a try hope it works', 'how to do scrum meeting', 'yashdeep', date('now'));
+INSERT INTO threads(forum_id, title, text, author, timestamp) VALUES (2, 'what to do next', 'just take rest and enjoy', 'david', date('now'));
 
-INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'post for first thread - first forum', 'paudkddl', date('now'));
-INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'post for second thread - first forum', 'jack', date('now'));
-INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'post for first thread - second forum', 'ames', date('now'));
-INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'post for second thread - second forum', 'ines', date('now'));
+INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'i hope this post works', 'david', date('now'));
+INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'lets give it another try', 'david', date('now'));
+INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'i know what you mean it should work', 'yashdeep', date('now'));
+INSERT INTO posts(thread_id, text, poster, timestamp) VALUES (1, 'i am not sure though', 'yashdeep', date('now'));
